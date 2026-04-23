388dc2aa7008bdce5b7cd3014c461112cf32b0f8
Aryan <yourgmail@gmail.com>
2026-04-23 14:59:31 +0530

    render port fix

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api", grievanceRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
